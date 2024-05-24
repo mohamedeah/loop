f0f0d274f5f60c0a0c4a28d33b5752273e68c819
unknown <m_hafez1989@hotmail.com>
2024-05-24 20:14:57 +0200

    add the task

---
+++ loop.py
@@ -0,0 +1,11 @@
+numbers = [12,75,150,180,145,525,50]
+# new_numbers = []
+# for i in numbers:
+#     new_numbers.append(i/5)
+# print(new_numbers)
+
+new_numbers2 = []
+for number in numbers:
+    if number < 150:
+        print(new_numbers2.append(number))
+print(new_numbers2)
