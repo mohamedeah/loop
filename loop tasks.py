a52e38118fbbe42b1bea0e65d7391a4086151d4f
unknown <m_hafez1989@hotmail.com>
2024-06-07 19:07:26 +0200

    add the second task

---
+++ loop tasks.py
@@ -0,0 +1,132 @@
+#question 2
+# number = input("enter number :")
+# for num in number:
+#     print(num)
+
+
+# number = input("enter number :")
+# count = 0
+# while number != 0:
+#     number //= 10
+#     count += 1
+# print("Number of digits: " + str(count))
+
+
+# num = float(input("enter your number :"))
+# count = 0
+# while num != 0:
+#     num = num // 10
+#     count = count + 1
+# print(count)
+
+# for number in range(3):
+#   print(number)
+# for i in range(2):
+#   print("First")
+#   print("Second")
+
+# password = "SecretWord"
+# guess = "SecretWord"
+# print(guess == password)
+  
+# password = "SecretWord"
+# guess = input()
+# while guess != password:  
+#   guess = input() 
+# print("Access Granted")
+
+# for box in range(50):
+#     print("Package A")
+
+
+# def hello(name):
+#     new_name = name.capitalize()
+#     return f'hello {new_name}'
+# print(hello('omar'))
+
+# def check(name):
+#     if name.islower():
+#         print(f"the {name} is lower case")
+#     else: 
+#         print(f"the {name} is upper case")
+# x = input("enter your name :")
+# y = input("enter your name :")
+# z = input("enter your name :")
+# check(x)
+# check(y)
+# check(z)
+
+# def sum_num(num1,num2):
+#     print(num1+num2)
+
+# def minus(num1,num2):
+#     print(num1-num2)    
+
+# def division(num1,num2):
+#     print(num1/num2)
+
+# def multi(num1,num2):
+#     print(num1*num2)
+
+# num1 = float(input("enter number 1 :"))
+# num2 = float(input("enter number 2 :"))
+# ope = input("enter the operation + - * / :")
+
+# if ope == "+":
+#     sum_num(num1,num2)
+# elif ope == "-":
+#     minus(num1,num2)
+# elif ope == "/":
+#     division(num1,num2)
+# elif ope == "*":
+#     multi(num1,num2)
+
+
+# def sum_num(num1,num2):
+#     print(num1+num2)
+
+# x = float(input("enter number 1 :"))
+# y = float(input("enter number 2 :"))
+
+# sum_num(x,y)
+
+
+
+
+
+# age = 16
+# if age < 18: print("Apply Discount")
+# print("Proceed to Payment")
+
+
+
+
+# list1 = [10,20,30,40,50]
+# list1.reverse()
+# for i in list1:
+#     print(i)
+
+# list1 = [10,20,30,40,50]
+# new = list1[::-1]
+# for z in new:
+#     print(z)
+
+# list1 = [10,20,30,40,50]
+# for i in list1[::-1]:
+#     print(i)
+  
+# name = "omar"
+# new_name = ""
+# for nam in name:
+#     new_name += nam.upper()
+# print(new_name)
+
+# names = ["mohamed","amr","adel","dina"]
+# names1 = []
+# for i in names:
+#     names1.append(i.capitalize())
+# print(names1)
+
+for x in range(200):
+  print("Repeat")
+print("Stop")
